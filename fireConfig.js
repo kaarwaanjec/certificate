f6796be9e179412088dc51ba66b111421471a90a
ra-jkunwar <62212221+ra-jkunwar@users.noreply.github.com>
2022-04-17 22:32:00 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBiteg1EJBLeI1beLnOMajr4gQGq6GWczU",
+  authDomain: "kwa-cert-verification.firebaseapp.com",
+  databaseURL: "https://kwa-cert-verification-default-rtdb.firebaseio.com",
+  projectId: "kwa-cert-verification",
+  storageBucket: "kwa-cert-verification.appspot.com",
+  messagingSenderId: "89282994526",
+  appId: "1:89282994526:web:c4a459af8a85216dd3efcf",
+  measurementId: "G-JBRB86QMQ9"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
